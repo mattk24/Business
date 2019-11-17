d548d1ea963e61243c5f646a1d2528f317ebe3f4
mattk24 <48831464+mattk24@users.noreply.github.com>
2019-11-17 09:54:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $("h1").css("color", "blue");
 $(".pricing").css("color", "blue");
 $(".self").css("color", "blue");
 $("button").css("font-size", "11pt");
+$("button").css("font-weight", "bold");
 $("a").css("color", "gold");
 $("button").css("background-color", "blue");
 $(".back-end").css("color", "blue");
